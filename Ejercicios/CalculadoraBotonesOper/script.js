0b188f6d3bc8dda9cff541315c1084dfeaa10543
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-08-02 19:30:10 +0200

    Calculadora terminada

--- Ejercicios/CalculadoraBotonesOper/script.js
+++ Ejercicios/CalculadoraBotonesOper/script.js
@@ -10,5 +10,36 @@ function suma(){
     var c = +a + +b;
     alert('El resultado de sumar ' + a + ' y ' + b + ' es: ' + c);
 }
+function resta(){
+    var a = prompt('Escribe el primer número: ');
+    var b = prompt('Escribe el segundo número: ');
+    var c = +a - +b;
+    alert('El resultado de restar ' + a + ' y ' + b + ' es: ' + c);
+}
+function multiplicacion(){
+    var a = prompt('Escribe el primer número: ');
+    var b = prompt('Escribe el segundo número: ');
+    var c = +a * +b;
+    alert('El resultado de multiplicar ' + a + ' y ' + b + ' es: ' + c);
+}
+function division(){
+    var a = prompt('Escribe el primer número: ');
+    var b = prompt('Escribe el segundo número: ');
+    var c = +a / +b;
+    alert('El resultado de dividir ' + a + ' y ' + b + ' es: ' + c);
+}
+function modulo(){
+    var a = prompt('Escribe un número: ');
+    if(+a % 2 == 0){
+        alert('El número ' + a + ' es par.');
+    }
+    else{
+        alert('El número ' + a + ' es impar.');
+    }
+}
 
 mySumaBtn.addEventListener('click',suma);
+myRestaBtn.addEventListener('click',resta);
+myMultBtn.addEventListener('click',multiplicacion);
+myDivBtn.addEventListener('click',division);
+myModBtn.addEventListener('click',modulo);
