d8d69c5050ef7ae1d837805f3e93176ff338ee25
Mikel Arina Marcos <mikel.arina@estudiantat.upc.edu>
2022-08-06 12:14:11 +0200

    Cambios en main.js

--- EjemplosProyectos/JS/main.js
+++ EjemplosProyectos/JS/main.js
@@ -4,3 +4,19 @@ const submit = document.querySelector('.search-submit')
 const input = document.querySelector('.search-input')
 const view = document.querySelector('#view')
 const gifs = await getGifs()
+
+renderGifs(gifs)
+
+submit.addEventListener('click', async function(e){
+    e.preventDefault()
+    const keyword = input.value
+    if(keyword){
+        const res = await getGifs({keyword})
+        if(res){
+            view.innerHTML = ''
+            input.value = ''
+            renderGifs(res)
+        }
+    }
+})
+
